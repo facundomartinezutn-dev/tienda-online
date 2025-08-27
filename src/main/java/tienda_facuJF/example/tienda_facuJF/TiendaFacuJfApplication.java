7379f62ae38a7bef256bd9e52725082b98b48a04
Facu24fm <Facundolarroza2017@gmail.com>
2025-08-27 09:21:21 -0300

    Commit inicial: Estructura de proyecto con Spring Boot

---
+++ src/main/java/tienda_facuJF/example/tienda_facuJF/TiendaFacuJfApplication.java
@@ -0,0 +1,13 @@
+package tienda_facuJF.example.tienda_facuJF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFacuJfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFacuJfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tienda_facuJF/example/tienda_facuJF/TiendaFacuJfApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda_facuJF.example.tienda_facuJF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFacuJfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
